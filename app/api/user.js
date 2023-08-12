d0b49cecc307fcf2d0d04a7bc87067d4c4dc333b
mh-anwar <mohammad.anwar2939@gmail.com>
2023-08-12 15:54:37 -0400

    Add user schema

---
+++ app/api/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+import { Schema, model } from 'mongoose';
+const userSchema = Schema({
+	email: {
+		type: String,
+		required: true,
+	},
+	firstName: {
+		type: String,
+		required: true,
+	},
+	lastName: {
+		type: String,
+		required: true,
+	},
+	age: {
+		type: Number,
+		required: true,
+	},
+	type: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	interests: {
+		type: [String],
+		required: false,
+	},
+	achievements: {
+		type: [String],
+		required: false,
+	},
+	level: {
+		type: Number,
+		default: false,
+	},
+	appointments: [{ type: Schema.ObjectId, ref: 'Appointment' }],
+	chat: [{ type: Schema.ObjectId, ref: 'Chat' }],
+});
+
+const User = model('User', userSchema);
+
+export default User;
