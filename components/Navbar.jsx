94c89285c8b81776aece2636be7a86cff9240ea2
m-shao <s201083816@ddsbstudent.ca>
2023-08-13 00:19:30 -0400

    navbar returns

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,9 +16,12 @@ const Navbar = () => {
 
     return (
         <div>
-
-        
-            <nav className="bottom-0 w-full max-w-lg py-2 bg-gray-50">
+            <div className='p-6 rounded-full'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                    </svg>
+            </div>
+            <nav className="fixed bottom-0 w-full max-w-lg py-2 bg-gray-50">
                 <div className="container">
                     <ul className ="flex justify-between px-4">
                         <li className={'p-4 rounded-full text-gray-400 ' + (page === '/portal' && 'bg-gray-200 text-gray-600')}>
