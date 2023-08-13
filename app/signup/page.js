0bcb3b0f8c59c6d218b74e10ca925d32422e2970
mh-anwar <mohammad.anwar2939@gmail.com>
2023-08-13 01:52:43 -0400

    Integrate frontend

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,9 +1,25 @@
-import React from 'react'
+'use client';
+import React, { useEffect } from 'react';
 
 const SignupPage = () => {
-  return (
-    <div>SignupPage</div>
-  )
-}
+    useEffect(() => {
+        fetch('./api/user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: 'msso',
+                firstName: 'sss',
+                lastName: 'sss',
+                password: 'sss',
+                type: 'sss',
+                age: 15,
+                bio: '2222',
+            }),
+        });
+    }, []);
+    return <div>SignupPage</div>;
+};
 
-export default SignupPage
+export default SignupPage;
