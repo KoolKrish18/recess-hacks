4046d0c41a2568e8310f67b81c226e874300dd89
Reese Chong <dev.reese.chong@gmail.com>
2023-08-13 00:27:23 -0400

    added hands to front page

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import HandsHolding from 'public/handsholding.png'
 import GradientButton from '@components/GradientButton'
 
 const SignupPage = () => {
@@ -7,17 +8,16 @@ const SignupPage = () => {
     <div className="flex flex-col content-center min-h-screen">
       <div className="flex justify-center">
         <Image
-          src="/images/landing-page.png"
+          src={HandsHolding}
           alt="Landing Page"
-          height={300}
-          width={300}
+          width="100%"
           quality={100}
         />
       </div>
       <div className="flex justify-center pt-[1em]">
         <div className='w-[80%] flex flex-col p-10 text-center'>
-            <h1 className='flex flex-wrap text-2xl font-bold'>
-                Find Your Legacy With Us
+            <h1 className='flex flex-wrap text-5xl font-bold'>
+                Find Your Legacy
             </h1>
             <p>Join with us and socialize across generations.</p>
             <GradientButton text="Get Started"/>
