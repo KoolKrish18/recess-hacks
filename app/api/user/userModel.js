258ac40671a0d4842fc96c65003f2063db07092f
mh-anwar <mohammad.anwar2939@gmail.com>
2023-08-12 22:46:57 -0400

    Add API routes

--- app/api/user/userModel.js
+++ app/api/user/userModel.js
@@ -1,46 +1,55 @@
-import { Schema, model } from 'mongoose';
+import mongoose, { Schema, model } from 'mongoose';
+
 const userSchema = Schema({
-	_id: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	firstName: {
-		type: String,
-		required: true,
-	},
-	lastName: {
-		type: String,
-		required: true,
-	},
-	age: {
-		type: Number,
-		required: true,
-	},
-	type: {
-		type: String,
-		required: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	interests: {
-		type: [String],
-		required: false,
-	},
-	achievements: {
-		type: [String],
-		required: false,
-	},
-	level: {
-		type: Number,
-		default: false,
-	},
-	appointments: [{ type: Schema.ObjectId, ref: 'Appointment' }],
-	chat: [{ type: Schema.ObjectId, ref: 'Chat' }],
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    bio: {
+        type: String,
+        required: true,
+    },
+    favoriteChats: [{ type: Schema.ObjectId, ref: 'Chat', required: false }],
+    interests: {
+        type: [String],
+        required: false,
+    },
+    achievements: {
+        type: [String],
+        required: false,
+    },
+    level: {
+        type: Number,
+        default: 0,
+        required: false,
+    },
+    appointments: [
+        { type: Schema.ObjectId, ref: 'Appointment', required: false },
+    ],
+    chat: [{ type: Schema.ObjectId, ref: 'Chat', required: false }],
 });
 
-const User = model('User', userSchema);
-
-export default User;
+const UserModel =
+    mongoose.models.UserModel || mongoose.model('UserModel', userSchema);
+export { UserModel };
