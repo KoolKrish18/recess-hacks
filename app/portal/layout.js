6467e0bc2c60c65669acc89ac3553fbcf1b6687c
Reese Chong <dev.reese.chong@gmail.com>
2023-08-13 18:01:30 -0400

    Redirect using useeffect

--- app/portal/layout.js
+++ app/portal/layout.js
@@ -1,6 +1,19 @@
+'use client';
+
 import Navbar from '@components/Navbar';
+import { useEffect } from 'react';
+import { redirect } from 'next/navigation';
 
 const PortalLayout = ({ children }) => {
+    useEffect(() => {
+        if (
+            !localStorage.getItem('email') &&
+            !localStorage.getItem('password')
+        ) {
+            redirect('/login');
+        }
+    });
+
     return (
         <div className="flex flex-col h-full">
             <div className="flex-1">{children}</div>
