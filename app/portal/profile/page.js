7512206eb8dcb953868d596f25fb8a43b5679692
mh-anwar <mohammad.anwar2939@gmail.com>
2023-08-13 21:17:14 -0400

    Integrate profile with backend

--- app/portal/profile/page.js
+++ app/portal/profile/page.js
@@ -1,62 +1,18 @@
 'use client';
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
 
-const ProfilePage = () => {
-    const [profileData, setProfileData] = useState({
-        firstName: '',
-        lastName: '',
-        bio: '',
-        interests: [],
-        age: 0,
-        email: '',
+export default function ProfilePage() {
+    const router = useRouter();
+    const userEmail = localStorage.getItem('email');
+    useEffect(() => {
+        router.push('/portal/profile/' + userEmail);
     });
 
-    let profData = () => {
-        fetch('/api/user?email=' + localStorage.getItem('email'))
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                setProfileData(data.user);
-                return data;
-            });
-    };
-    useEffect(() => {
-        profData();
-    }, []);
     return (
-        <div className="container p-6 mx-auto">
-            <div className="flex flex-col items-center space-y-6">
-                <Image
-                    src="https://innocenceproject.org/wp-content/uploads/2023/02/AP_22082506803469-scaled-1.jpg"
-                    alt={`${profileData.name}'s Profile`}
-                    width={128}
-                    height={128}
-                    className="w-32 h-32 rounded-full"
-                />
-                <h1 className="text-2xl font-semibold">
-                    {profileData.firstName} {profileData.lastName}
-                </h1>
-                <p className="text-gray-600">{profileData.bio}</p>
-                <div className="flex space-x-2">
-                    {profileData.interests.map((interest, index) => (
-                        <span
-                            key={index}
-                            className="px-2 py-1 text-sm bg-gray-200 rounded"
-                        >
-                            {interest}
-                        </span>
-                    ))}
-                </div>
-
-                <p className="text-gray-600">Age: {profileData.age}</p>
-                <button className="px-4 py-2 text-white bg-blue-500 rounded-md">
-                    Send Message
-                </button>
-                <p className="text-gray-600">Email: {profileData.email}</p>
-            </div>
+        <div>
+            <Link href={'/portal/profile/' + userEmail} />
         </div>
     );
-};
-
-export default ProfilePage;
+}
